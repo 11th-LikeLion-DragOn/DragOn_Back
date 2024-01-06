ddc7a55774c66e7ebef37b686426dcda33d1eb65
yewon <shimyewon@ewhain.net>
2024-01-07 00:08:16 +0900

    [feat] 반응, 여부 변경

--- main/models.py
+++ main/models.py
@@ -3,32 +3,50 @@ from django.db import models
 from accounts.models import *
 from datetime import datetime, timedelta
 from django.utils import timezone
-
-class Reaction(models.Model):
-    #challenge = models.ForeignKey('Challenge', on_delete=models.CASCADE, related_name='reactions')
-    emgoji = models.IntegerField()
-    check = models.BooleanField(default=False)
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Challenge(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.TextField(null=False)
-    period = models.IntegerField() #기간, 며칠동안
-    time = models.IntegerField() #여의주 충전 7일 관련.. 사용자 직접 입력 x 
-    reaction = models.ManyToManyField(Reaction, related_name='challenges', blank=True)
+    name = models.TextField(null=False) 
+    period = models.IntegerField(blank=False, null=False) #기간, 며칠동안
+    #time = models.IntegerField(default=7, editable=False) #여의주 충전 7일 관련.. 사용자 직접 입력 x 
     created_at = models.DateTimeField(auto_now_add=True)
-    ended_at = models.DateTimeField(blank=False, default=timezone.now) #period 더하면 ended_at 
+    #ended_at = models.DateTimeField(null=True, blank=True)
+    good=models.ManyToManyField(User, related_name="challgege_good")
+    question=models.ManyToManyField(User, related_name="challgege_question")
+    fighting=models.ManyToManyField(User, related_name="challgege_fighting")
+    fire=models.ManyToManyField(User, related_name="challgege_fire")
+    mark=models.ManyToManyField(User, related_name="challgege_mark")
+    heart=models.ManyToManyField(User, related_name="challgege_heart")
 
 class Goals(models.Model):
     challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, related_name='goals')
     content = models.TextField(blank=True)
     activate = models.BooleanField(default=True)
 
+
+
 class Achieve(models.Model):
-    goal = models.ForeignKey(Goals, on_delete=models.CASCADE)
-    period = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    goal = models.ForeignKey(Goals, on_delete=models.CASCADE, related_name='achieves')
     is_done = models.BooleanField(default=False)
     today = models.BooleanField(default=True)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField(null=True, blank=True, editable=False)
+
+@receiver(post_save, sender=Goals)
+def create_initial_achieve(sender, instance, created, **kwargs):
+    if created:
+        Achieve.objects.create(goal=instance)
+
+@receiver(post_save, sender=Achieve)
+def create_daily_achieve(sender, instance, created, **kwargs):
+    if created and not instance.today:
+        # 오후 10시 50분에 실행되도록 설정
+        scheduled_time = timezone.now().replace(hour=22, minute=39, second=0, microsecond=0)
+
+        # 현재 시간이 오후 10시 50분 이후인 경우에만 추가로 Achieve 생성
+        if timezone.now() > scheduled_time:
+            Achieve.objects.create(goal=instance.goal, today=False, date=timezone.now().date())
 
 class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -47,4 +65,7 @@ class Recomments(models.Model):
 
 class Ball(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_balls')
-    count = models.IntegerField()
+    challenge=models.ForeignKey(Challenge, on_delete=models.CASCADE, null=True) #챌린지 한 개당 여의주 한개 
+    time = models.IntegerField(default=1)
+    count = models.IntegerField(default=0)
+    is_use=models.BooleanField(default=False)
--- main/urls.py
+++ main/urls.py
@@ -5,20 +5,17 @@ app_name = 'main'
 
 urlpatterns = [
     #path("challenge/", .as_view()),
-    path("challengeadd/<int:user_pk>/", ChallengeAddView.as_view()), #챌린지 생성하기
-    path("challengeList/<int:user_pk>/", ChallengeListView.as_view()), #챌린지 목록 조회
-    path("challenge/<int:pk>/edit/<int:challenge_pk>/",ChallengeEditView.as_view()), #챌린지 수정하기
+    path("challengeadd/", ChallengeAddView.as_view()), #챌린지 생성하기
+    path("challengelist/", ChallengeListView.as_view()), #챌린지 목록 조회 (관련 목표들도 같이)
     path("goaladd/<int:challenge_pk>/", GoalAddView.as_view()), #목표 생성하기
-    path("challengeCheck/<int:challenge_pk>/<int:goal_pk>/", CheckAchievement.as_view()), #목표 달성 후 체크
-    #path("goal/", .as_view()),
-    #path("<int:challenge_pk>/?type=int/", .as_view()),
-    path("challenge/<int:pk>/commentList/", CommentListView.as_view()), #챌린지 댓글 모아보기
+    path("goal/<int:goal_pk>/", AchievementView.as_view()), #목표 달성 후 체크
+    #path("challenge/reaction/<int:challenge_id>/",  ReactionView.as_view()), #챌린지 반응 달기
+    path("challenge/<int:pk>/commentlist/", CommentListView.as_view()), #챌린지 댓글 모아보기
     path("challenge/<int:pk>/comment/", CommentView.as_view()), #챌린지 댓글 작성하기
-    path("challenge/<int:pk>/comments/<int:comment_pk>/", CommentRView.as_view()), #챌린지 댓글 수정, 삭제
+    path("challenge/<int:pk>/comment/<int:comment_pk>/", CommentRView.as_view()), #챌린지 댓글 수정, 삭제
     path("comment/<int:comment_pk>/recomment/", RecommentView.as_view()), #챌린지 대댓글
-    path("comment/<int:comment_pk>/recomment/<int:recomment_pk>/", RecommentRView.as_view()), #챌린지 대댓글 수정, 삭제
     path("challenge/<int:challenge_pk>/", DeleteChallengeView.as_view()), #챌린지 그만두기
-    path("goal/<int:goal_pk>/", DeleteGoalView.as_view()), #목표 삭제하기
-
+    path("goaldelete/<int:goal_pk>/", DeleteGoalView.as_view()), #목표 삭제하기
+    path('reactions/<int:challenge_id>/<str:emotion_type>/', ReactionView.as_view())
 
 ]
